fdb6ed4a7cb3b4477e2e69ebc121c3edabc37919
nadiyakm <kmnadiya003@gmail.com>
2026-01-23 11:50:22 +0530

    samesite

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -39,7 +39,7 @@ exports.login = async (req, res) => {
 	res.cookie('userToken', token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production', // only secure in prod
-		sameSite: 'lax',
+		sameSite: 'none',
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 
